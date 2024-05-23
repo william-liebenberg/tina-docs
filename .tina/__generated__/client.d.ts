6c6aefc882b8b9cf3ba100cfa9a60c0c0df75c65
William Liebenberg [SSW • Microsoft MVP] <williamliebenberg@ssw.com.au>
2024-05-24 01:06:42 +1200

    include intro in the tina document query

--- .tina/__generated__/client.d.ts
+++ .tina/__generated__/client.d.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'e7deb40ea3dac6e0f7cc84d14dc68fdebb1b444c', queries,  });
 export default client;
   
